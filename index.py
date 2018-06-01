79d27cce30094be29c71ad5190368e969ac31b33
YunxiaoXu <xuyunxiao2001@gmail.com>
2018-06-01 18:37:36 +0800

    modify post

--- index.py
+++ index.py
@@ -6,19 +6,24 @@ def main(argv):
 	argv = unquote(argv).split("&")
 	argv_list = [a.split("=", 1) for a in argv]
 	argv_dict = {}
-	for key, value in argv_list:
-		argv_dict[key] = value
-	json_data = json.dumps(argv_dict)
+	try:
+		for key, value in argv_list:
+			argv_dict[key] = value
+		json_data = json.dumps(argv_dict)
+	except:
+		json_data = json.dumps(None)
 
 	pathname = os.path.join(os.path.dirname(__file__), "log")
 	if not os.path.exists(pathname):
 		os.mkdir(pathname)
 	filename = os.path.join(pathname, "log.txt")
 
-	open(filename, 'a').write(";".join(argv)+"\n")
-	return json_data
 
-if len(sys.argv) == 2:
-	print main(sys.argv[1])
-else:
-	print "error: no arguments"
+	if "time" in argv_dict:
+		open(filename, 'a').write(argv_dict["time"]+" ")
+	for key in argv_dict:
+		if key!="time":
+			open(filename, 'a').write("{}: {}; ".format(key, argv_dict[key]))
+	open(filename, 'a').write("\n")
+	#open(filename, 'a').write("; ".join(argv).replace("=",": ")+"\n")
+	return json_data
